afdf6e8ddd657b3dd206feb185a1c22b4bf524c3
Suraj Kumar <155295926+Suraj766325@users.noreply.github.com>
2024-05-11 21:09:27 +0530

    Add files via upload

---
+++ leetcode_81.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> v={8,10,2,3,5,7};
+    int n=v.size(),l=0,r=n-1,mid,idx=-1,target=0;
+    while(l<=r)
+        {
+            mid=l+(r-l)/2;
+            if(target==v[mid]) 
+            {
+                idx=mid;
+                break;
+            }
+            else if(v[mid]==v[r] && v[mid]==v[l])
+            {
+                l++;
+                r--;
+            }
+            else if(v[mid]<=v[r])
+            {
+                if(target>=v[mid] && target<=v[r]) l=mid+1;
+                else                               r=mid-1;
+            }
+            else
+            {
+                if(target>=v[l] && target<=v[mid]) r=mid-1;
+                else                               l=mid+1;
+            }
+        }
+        cout<<idx;
+}
